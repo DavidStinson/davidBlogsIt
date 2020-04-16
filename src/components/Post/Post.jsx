426842b7bf5ce874a6e1aa9560b49b71501f6bd4
David Stinson <david.stinson.5@gmail.com>
2020-04-16 14:05:14 -0500

    finalize initial read

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -5,11 +5,11 @@ const Post = ({post, user}) => {
   return (
     <React.Fragment>
       <h3>{post.title}</h3>
-      <p>{post.author.name}</p>
+      <p>{post.author}</p>
       <h4>{post.topic}</h4>
       <br />
       {post.content.map(content =>
-        <p key={content._id}>{content.content}</p>
+        <p key={content._id}>{content.data}</p>
       )}
     </React.Fragment>
   );
