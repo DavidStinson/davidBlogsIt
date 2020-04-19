7de289fba981899a85d4bd0b423dc936def4426b
David Stinson <david.stinson.5@gmail.com>
2020-04-19 13:39:55 -0500

    finish topic controller

---
+++ controllers/topics.js
@@ -0,0 +1,38 @@
+var Topic = require("../models/topic");
+
+module.exports = {
+  index,
+  create,
+  delete: deleteOne,
+};
+
+async function index(req, res) {
+  try {
+    const topics = await Topic.find({});
+    res.status(200).json(topics);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+async function create(req, res) {
+  console.log("user: ", req.user);
+  console.log("post: ", req.body)
+  try {
+    const topic = await Topic.create(req.body);
+    res.status(201).json(topic);
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
+}
+
+async function deleteOne(req, res) {
+  try{
+    const deletedTopic = await Topic.findByIdAndRemove(req.params.id);
+    res.status(200).json(deletedTopic);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
+}
